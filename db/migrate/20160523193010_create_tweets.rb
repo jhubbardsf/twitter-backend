c68ec5a3c2fa3792521610a0cd0d293fc942aa12
Josh Hubbard <jhubbardsf@gmail.com>
2016-05-23 15:41:34 -0400

    Added in scheduler and created the tweets table.

---
+++ db/migrate/20160523193010_create_tweets.rb
@@ -0,0 +1,16 @@
+class CreateTweets < ActiveRecord::Migration[5.0]
+  def change
+    create_table :tweets do |t|
+      t.string :topic
+      t.string :text
+      t.string :user_name
+      t.string :screen_name
+      t.datetime :create_date
+      t.string :link
+
+      t.timestamps
+    end
+
+    add_index(:tweets, :topic)
+  end
+end
