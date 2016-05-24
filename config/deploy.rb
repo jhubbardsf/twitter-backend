b699e8ca114571f82ba6cd1293bea8cff8fe7829
Josh Hubbard <jhubbardsf@gmail.com>
2016-05-23 23:04:12 -0400

    Removed precompiling assets from the API service.

--- config/deploy.rb
+++ config/deploy.rb
@@ -77,7 +77,6 @@ namespace :deploy do
   end
 
   before :starting,     :check_revision
-  after  :finishing,    :compile_assets
   after  :finishing,    :cleanup
   after  :finishing,    :restart
 end
