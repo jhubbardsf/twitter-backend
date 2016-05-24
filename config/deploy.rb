00650ebb30b10e43951cf95c60207ee00151789d
Josh Hubbard <jhubbardsf@gmail.com>
2016-05-24 03:37:15 -0400

    Changing the cleanup number for backend.

--- config/deploy.rb
+++ config/deploy.rb
@@ -27,7 +27,7 @@ set :puma_init_active_record, true  # Change to false when not using ActiveRecor
 # set :branch,        :master
 # set :format,        :pretty
 # set :log_level,     :debug
-# set :keep_releases, 5
+set :keep_releases, 5
 
 ## Linked Files & Directories (Default None):
 #set :linked_files, %w{config/secrets.yml .env}
